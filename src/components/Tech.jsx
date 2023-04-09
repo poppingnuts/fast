af2e0d08093baca2922f1435cdf42ade3e2a61ed
poppingnuts <poppingnuts0@gmail.com>
2023-04-09 18:19:59 +0530

    whts

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { technologies } from "../constants";
 import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../style";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <>
+    
+     <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Tech We Use</h2>
+        {/* <p className={styles.sectionSubText}>We Use</p> */}
+      </motion.div>
+    <div className="flex flex-row flex-wrap justify-center mt-10 gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
+      </>
   );
 };
 
